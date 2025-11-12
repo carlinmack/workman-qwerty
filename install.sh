7fd7d69d85b072d0cc814df5a49e2ee91ef0069b
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2025-11-12 13:04:17 +0100

    improve install.sh
    * now compliant with shellcheck
    * reuse $CONF variable instead of rewriting
    * alphabetise directories

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 NAME=xkb-backup.tar.gz
 CONF=/usr/share/X11/xkb
@@ -13,11 +13,11 @@ fi
 
 
 echo "Backing up current configuration in $PWD/$NAME"
-tar zcf $NAME /usr/share/X11/xkb --absolute-names
-DIRECTORY_OWNER=$(namei -o $PWD | tail -n 1 | awk '{if ($2) print $2}')
-chown $DIRECTORY_OWNER -R $NAME
+tar zcf $NAME $CONF --absolute-names
+DIRECTORY_OWNER=$(namei -o "$PWD" | tail -n 1 | awk '{if ($2) print $2}')
+chown "$DIRECTORY_OWNER" -R $NAME
 
 echo "Copying Dvorak-Qwerty configuration"
-cp -R ./types ./symbols/ ./rules/  $CONF
+cp -R ./rules/ ./symbols/ ./types $CONF
 
 exit 0
